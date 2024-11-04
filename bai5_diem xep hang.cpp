730dd3ba3d191b1152bc6c686cb5a9c16e63cd5a
mosby1596 <hoang159609@gmail.com>
2024-11-04 16:51:36 +0700

    Add project files.

---
+++ bai5_diem xep hang.cpp
@@ -0,0 +1,22 @@
+// bai5_diem xep hang.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "chuong trinh diem xep hang\n";
+	double diem;
+	cout << "diem:";
+	if (diem < 5){
+		cout << "yeu";
+	}
+	else if (diem > 5)
+	{
+		cout << "TB";
+	}
+	else if (diem > 6)
+		cout << "kha";
+	else {
+
+
